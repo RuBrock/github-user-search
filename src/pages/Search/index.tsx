56d353b7af1d04721565c23ad7daf2a379725cc8
Rubens Brock <r.brocksilva@yahoo.com.br>
2020-10-24 21:02:21 -0300

    Search layout finished

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import ButtonAction from '../../core/components/ButtonAction';
+import './styles.scss';
 
-const Search = () => (
-    <div>
-        <h1>Search</h1>
-    </div>
-)
+const Search = () => {
+    return (
+        <div className="search-container">
+            <div className="search-form">
+                <form>
+                    <h1>Encontre um perfil Github</h1>
+
+                    <input type="text"/>
+
+                    <div className="btn-encontrar">
+                        <ButtonAction text="Encontrar" />
+                    </div>
+                </form>
+            </div>
+
+            <div className="search-results">
+
+            </div>
+        </div>
+    );
+}
 
 export default Search;
